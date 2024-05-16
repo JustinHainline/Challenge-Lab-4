d9d39795b3c5368146ff8b18189d1b24adf3f4ed
Justin Hainline <Justin.Hainline@mssa.newhorizons.com>
2024-05-16 11:17:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Challenge_Lab_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a letter: ");
+            string letter = (Console.ReadLine());
+
+            Console.WriteLine("Enter desired width: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = width; i >= 1; i--)
+            {
+                for (int j = 0; j < i ; j++)
+                {
+                    Console.WriteLine(letter);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
